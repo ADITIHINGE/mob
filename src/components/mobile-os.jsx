2496bfac2dff94e7dd7888d3612d436a7a51d1b1
Aditi Hinge <aditihinge287@gmail.com>
2022-05-18 13:58:43 +0530

    fir commit

---
+++ src/components/mobile-os.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Mob = () => {
+    return  <div>
+    <h1>
+    Mobile Operating System
+    </h1>
+    <div>
+    <li>
+    Android
+    </li>
+    <li>
+    Blackberry
+    </li>
+    <li>
+     iPhone
+    </li>
+    <li>
+    Windows Phone
+    </li>
+    </div>
+    <h1>
+    Mobile Manufactures
+    </h1>
+    <li>
+    Samsung
+    </li>
+    <li>
+    HTC
+    </li>
+    <li>
+     Micromax
+    </li>
+    <li>
+    Apple
+    </li>
+    
+        </div>
+    
+
+};
+export default Mob;
